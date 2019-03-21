0dc9e040772fe48735456ddc89600fe214b45f55
XiaoZQ <xzq1047@163.com>
2019-03-22 07:47:41 +0800

    2019.3.17 updata P9

--- P17/p17.py
+++ P17/p17.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
@@ -29,9 +30,22 @@ train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
 
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()   # 显示图片，继续运行程序
+plt.show()
+
 with tf.Session() as sess:
     sess.run(init)
     for i in range(1000):
         sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
         if i % 50 is 0:
-            print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+            # print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+            try:
+                ax.lines.remove(lines[0])
+            except Exception:
+                pass
+            predition_value = sess.run(predition, feed_dict={xs: x_data})
+            lines = ax.plot(x_data, predition_value, 'r-', lw=5)
+            plt.pause(0.1)
